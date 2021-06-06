7dd5a544ab0cc7c56b2f99eb646318ca4e4e9ede
CrispyPin <crispin@tasa.se>
2021-06-06 17:28:22 +0200

    cleanup main menu

--- ovr-utils/overlay_scripts/main_menu.gd
+++ ovr-utils/overlay_scripts/main_menu.gd
@@ -7,16 +7,17 @@ func _ready() -> void:
 	for o in Settings.s.overlays:
 		if o != "MainOverlay":
 			_add_overlay_to_list(o)
-	$VSplitContainer/Control/Overlays.visible = false
+	$MarginContainer/VSplitContainer/Control/Overlays.visible = false
 
 
 func _add_overlay_to_list(name):
 	var new = preload("res://ui/ListOverlayItem.tscn").instance()
 	new.overlay_name = name
-	$VSplitContainer/Control/Overlays.add_child(new)
+	$MarginContainer/VSplitContainer/Control/Overlays.add_child(new)
+
 
 func _remove_overlay_from_list(name):
-	$VSplitContainer/Control/Overlays.get_node(name).queue_free()
+	$MarginContainer/VSplitContainer/Control/Overlays.get_node(name).queue_free()
 
 
 func _on_GrabMode_toggled(state: bool) -> void:
@@ -24,9 +25,9 @@ func _on_GrabMode_toggled(state: bool) -> void:
 
 
 func _on_ShowOverlays_toggled(state: bool) -> void:
-	$VSplitContainer/Control/Overlays.visible = state
+	$MarginContainer/VSplitContainer/Control/Overlays.visible = state
 
 
-func _on_AddOverlay_pressed() -> void:
-	$VSplitContainer/Control/AddMenu.visible = !$VSplitContainer/Control/AddMenu.visible
+func _on_AddOverlay_toggled(state: bool) -> void:
+	$MarginContainer/VSplitContainer/Control/AddMenu.visible = state
 
