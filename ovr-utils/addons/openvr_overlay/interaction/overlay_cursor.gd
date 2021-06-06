4a89de6af996b3f0411f127361f2d897e0504ae4
CrispyPin <crispin@tasa.se>
2021-06-06 13:31:19 +0200

    start adding cursor move events

--- ovr-utils/addons/openvr_overlay/interaction/overlay_cursor.gd
+++ ovr-utils/addons/openvr_overlay/interaction/overlay_cursor.gd
@@ -5,6 +5,9 @@ var cursor_node  = preload("res://addons/openvr_overlay/interaction/Cursor.tscn"
 onready var viewport: Viewport = get_node("../../OverlayViewport")
 onready var _i = get_parent()
 
+var curr_pos: Vector2
+var prev_pos := Vector2(-1000, 1000)
+
 func _ready() -> void:
 	viewport.add_child(cursor_node)
 	get_parent().connect("trigger_on", self, "_trigger_on")
@@ -12,7 +15,10 @@ func _ready() -> void:
 
 
 func _process(_delta: float) -> void:
+	curr_pos = get_canvas_pos()
 	_update_cursor()
+	_send_move_event()
+	prev_pos = curr_pos
 
 
 #get canvas position of active controller
@@ -24,7 +30,6 @@ func get_canvas_pos() -> Vector2:
 			_i.tracker_nodes[_i.active_controller].translation)
 	var pos = Vector2(controller_local_pos.x, controller_local_pos.y)
 
-#	var overlay_size = OverlayInit.ovr_interface.get_render_targetsize()
 	var overlay_size = Vector2(2048, 2048)
 	# scale to pixels
 	pos *= overlay_size.x
@@ -39,9 +44,18 @@ func _update_cursor():
 	cursor_node.rect_position = get_canvas_pos()
 
 
+func _send_move_event():
+	var event = InputEventMouseMotion.new()
+	event.position = prev_pos
+	event.relative = curr_pos - prev_pos
+	event.speed = event.relative
+	viewport.input(event)
+
+
+
 func _send_click_event(state: bool):
 	var click_event = InputEventMouseButton.new()
-	click_event.position = get_canvas_pos()
+	click_event.position = curr_pos
 	click_event.pressed = state
 	click_event.button_index = 1
 	viewport.input(click_event)
